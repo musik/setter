26dde179a0a68a2d713253e4c5891c2f5a930b89
musik <860577@gmail.com>
2013-04-29 12:50:47 +0800

    actions

--- app/controllers/yun_controller.rb
+++ app/controllers/yun_controller.rb
@@ -7,7 +7,7 @@ class YunController < ApplicationController
     @results = {
       :CmdState=>1,
       :CmdXml=>{
-        :CmdCompanyUrl => @response.body,
+        :CmdCompanyUrl => @response.body.encode('utf-8','gbk'),
       }
     }
     render :soap=>@results
@@ -33,12 +33,18 @@ class YunController < ApplicationController
   end
   before_filter :dump_parameters,:remote_post,:except=>%(_generate_wsdl)
   private
+  def encode_params args,from,to
+    args.each do |k,v|
+      v.encode! to,from if v.is_a? String
+    end
+  end
   def remote_post
     url = "http://www.ynshangji.com/api/"
     #url = "http://localhost/wsdl.php"
-    @response = Typhoeus::Request.post "#{url}#{action_name}.asp",:params=> params
+    args = encode_params(params,'utf-8','gbk')
+    @response = Typhoeus::Request.post "#{url}#{action_name}.asp",:params=> args
     Rails.logger.debug @response.inspect
-    logger.debug @response.body
+    logger.debug @response.body.encode 'utf-8','GBK'
   end
   def dump_parameters
     Rails.logger.debug params.inspect
