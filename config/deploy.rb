21010233e35b0c15ff805c44a000a2eedfee0e7b
musik <860577@gmail.com>
2013-07-11 17:03:34 +0800

    ruby taobao

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,10 +3,10 @@ set :repository,  "git@github.com:musik/setter.git"
 
 set :scm, :git
 
-set :deploy_to, "/home/muzik/setter"
-role :web, "rho"                          # Your HTTP server, Apache/etc
-role :app, "rho"                          # This may be the same as your `Web` server
-role :db,  "rho", :primary => true # This is where Rails migrations will run
+set :deploy_to, "/dat/www/setter"
+role :web, "gxr"                          # Your HTTP server, Apache/etc
+role :app, "gxr"                          # This may be the same as your `Web` server
+role :db,  "gxr", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 set :user, "muzik"
 set :group, "muzik"
@@ -99,8 +99,8 @@ after 'deploy:restart', 'unicorn:restart' # app IS NOT preloaded
 #set :resque_service,'resque-sdmec'
 #require './lib/recipes/resque.rb'
 #before 'deploy:restart','resque:restart'
-#role :resque_worker, "rho"
-#role :resque_scheduler, "rho"
+#role :resque_worker, "gxr"
+#role :resque_scheduler, "gxr"
 #set :workers, { "update_keywords,update_items" => 1 }
 #require 'capistrano-resque'
 
